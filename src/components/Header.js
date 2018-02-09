86bc5e7458f0ecbd644385ff0736c6ee5ff37dde
Tatrakrad <riddick.taylor@gmail.com>
2018-02-09 13:11:09 -0600

    small placeholder content change

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,12 @@ import React from 'react'
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
-            <span className="icon fa-diamond"></span>
+            <span className="icon fa-leaf"></span>
         </div>
         <div className="content">
             <div className="inner">
                 <h1>Corn</h1>
-                <p>A thirst that unites and divides us, ground in the great mill that turns men into machines. Now in website form by <a href="http://jtriddick.com">JT Riddick</a> and based<br />
-                 on a template provided by html5up.net under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <p>A thirst that unites and divides us, ground in the great mill that turns men into machines.</p>
             </div>
         </div>
         <nav>
