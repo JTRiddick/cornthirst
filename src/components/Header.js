ac4793da21e3e3d8a3efae9ca1f517ce5de732de
Tatrakrad <riddick.taylor@gmail.com>
2019-04-25 23:00:34 -0500

    broken dependency updates wont build

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,10 +13,10 @@ const Header = (props) => (
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Begin</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>End</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>Why</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Who</a></li>
             </ul>
         </nav>
     </header>
